7186164f729c5868afe694649fdbfa136c4a7d29
davidtan <davidtan@thoughtworks.com>
2018-02-02 17:48:25 +0800

    Update function syntax to use ES6 arrow syntax

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const passport = require("passport");
 const auth = require("./auth");
 
-router.get("/", auth.required, function(req, res, next) {
+router.get("/", auth.required, (req, res, next) => {
   res.json("GET /user");
 });
 
